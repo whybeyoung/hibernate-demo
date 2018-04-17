df7480ba5659cd6a77cd1d5c02458e00a1094faf
xwliu <xwliu@iflytek.com>
2018-04-17 08:41:47 +0800

    modify k8s interface name

--- Sources/MiguIaasService/src/main/java/com/iflytek/iaas/service/impl/k8sServiceImpl.java
+++ Sources/MiguIaasService/src/main/java/com/iflytek/iaas/service/impl/k8sServiceImpl.java
@@ -186,7 +186,7 @@ public class k8sServiceImpl  implements K8SService {
                 container.setName(deployConfigDTO.getImgName()+"-"+Integer.toString(i));
                 container.setEnv(envs);
                 V1ContainerPort port = new V1ContainerPort();
-                port.setContainerPort(3306);
+                port.setContainerPort(deployConfigDTO.getContainerPort());
                 port.setProtocol("TCP");
                 List<V1ContainerPort> ports = new ArrayList<>();
                 ports.add(port);
@@ -470,13 +470,13 @@ public class k8sServiceImpl  implements K8SService {
     }
 
     @Override
-    public ServerInfoDTO getServerInfoByName(String hostName) throws IOException, ApiException{
+    public ServerInfoDTO getServerInfoByHostname(String hostname) throws IOException, ApiException{
         ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
         CoreV1Api coreV1Api = new CoreV1Api();
         ServerInfoDTO serverInfoDTO = new ServerInfoDTO();
         try {
-            V1Node node = coreV1Api.readNode(hostName, "", false, false);
+            V1Node node = coreV1Api.readNode(hostname, "", false, false);
 
             List<V1NodeAddress> addressList = node.getStatus().getAddresses();
             for (V1NodeAddress address : addressList) {
@@ -609,35 +609,6 @@ public class k8sServiceImpl  implements K8SService {
     public String getServerDiskByHostname(String hostName){
         String diskQuery = "sum(container_fs_limit_bytes{device=~\"^/.*$\",id=\"/\",kubernetes_io_hostname=~\""+ hostName +"\"})";
         return getHardRes(diskQuery);
-    }
-
-    @Override
-    public boolean testK8s()throws IOException, ApiException{
-        ApiClient client = Config.defaultClient();
-        Configuration.setDefaultApiClient(client);
-        CoreV1Api apiInstance = new CoreV1Api();
-        Map<String,String> labels = new HashMap<>();
-        labels.put("test","busybox-latest");
-        String namespace = "test"; // String | object name and auth scope, such as for teams and projects
-        String pretty = ""; // String | If 'true', then the output is pretty printed.
-        String _continue = ""; // String | The continue option should be set when retrieving more results from the server. Since this value is server defined, clients may only use the continue value from a previous query result with identical query parameters (except for the value of continue) and the server may reject a continue value it does not recognize. If the specified continue value is no longer valid whether due to expiration (generally five to fifteen minutes) or a configuration change on the server the server will respond with a 410 ResourceExpired error indicating the client must restart their list without the continue field. This field is not supported when watch is true. Clients may start a watch from the last resourceVersion value returned by the server and not miss any modifications.
-        String fieldSelector = ""; // String | A selector to restrict the list of returned objects by their fields. Defaults to everything.
-        Boolean includeUninitialized = false; // Boolean | If true, partially initialized resources are included in the response.
-//        String labelSelector = "test=node-exporter-latest";
-        String labelSelector = "ftp=yes"; // String | A selector to restrict the list of returned objects by their labels. Defaults to everything.
-        Integer limit = 56; // Integer | limit is a maximum number of responses to return for a list call. If more items exist, the server will set the `continue` field on the list metadata to a value that can be used with the same initial query to retrieve the next set of results. Setting a limit may return fewer than the requested amount of items (up to zero items) in the event all requested objects are filtered out and clients should only use the presence of the continue field to determine whether more results are available. Servers may choose not to support the limit argument and will return all of the available results. If limit is specified and the continue field is empty, clients may assume that no more results are available. This field is not supported if watch is true.  The server guarantees that the objects returned when using continue will be identical to issuing a single list call without a limit - that is, no objects created, modified, or deleted after the first request is issued will be included in any subsequent continued requests. This is sometimes referred to as a consistent snapshot, and ensures that a client that is using limit to receive smaller chunks of a very large result can ensure they see all possible objects. If objects are updated during a chunked list the version of the object that was present at the time the first list result was calculated is returned.
-        String resourceVersion = ""; // String | When specified with a watch call, shows changes that occur after that particular version of a resource. Defaults to changes from the beginning of history. When specified for list: - if unset, then the result is returned from remote storage based on quorum-read flag; - if it's 0, then we simply return what we currently have in cache, no guarantee; - if set to non zero, then the result is at least as fresh as given rv.
-        Integer timeoutSeconds = 56; // Integer | Timeout for the list/watch call.
-        Boolean watch = false; // Boolean | Watch for changes to the described resources and return them as a stream of add, update, and remove notifications. Specify resourceVersion.
-        try {
-//            V1PodList result = apiInstance.listNamespacedPod(namespace, pretty, _continue, fieldSelector, includeUninitialized, labelSelector, limit, resourceVersion, timeoutSeconds, watch);
-            V1NodeList result = apiInstance.listNode(pretty, _continue, fieldSelector, includeUninitialized, labelSelector, limit, resourceVersion, timeoutSeconds, watch);
-            System.out.println(result);
-        } catch (ApiException e) {
-            System.err.println("Exception when calling CoreV1Api#listNamespacedPod");
-            e.printStackTrace();
-        }
-        return false;
     }
 
     /**
